4ac1f32b0c7067083e35f668940b2a65ab2de1f7
Kal Bartal <kal.bartal@gmail.com>
2023-02-17 18:26:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Flask User Management API
+
+This repository contains code for a Flask API that can be used to manage user information. The API implements the following routes:
+
+* GET /users - to get list of all users in the database
+* GET /users/<int:user_id> - to get a specific user by id
+* POST /users - to create a new user
+* PUT /users/<int:user_id> - to update a specific user
+* DELETE /users/<int:user_id> - to delete a specific user
+
+## Requirements
+
+This API requires the following libraries:
+
+* Flask 0.12.2 or higher
+* SQLAlchemy 1.1.11 or higher
+* Werkzeug 0.12.2 or higher
+
+## Installation
+
+Clone the repository:
+
+```
+git clone https://github.com/username/flask-user-management.git
+```
+
+Navigate to the repository directory:
+
+```
+cd flask-user-management
+```
+
+Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Run the application:
+
+```
+python app.py
+```
+
+## Usage
+
+API endpoints can be tested with curl or any other HTTP client. Examples below use curl.
+
+**GET /users**
+
+Get all users in the database.
+
+```
+curl -X GET http://localhost:5000/users
+```
+
+**GET /users/<int:user_id>**
+
+Get a user with the specified ID.
+
+```
+curl -X GET http://localhost:5000/users/<user_id>
+```
+
+**POST /users**
+
+Add a new user to the database.
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "email": "john@example.com", "password": "password"}' http://localhost:5000/users
+```
+
+**PUT /users/<int:user_id>**
+
+Update a user with the specified ID.
+
+```
+curl -X PUT -H "Content-Type: application/json" -d '{"name": "John Smith", "email": "john@example.com", "password": "strongpassword"}' http://localhost:5000/users/<user_id>
+```
+
+**DELETE /users/<int:user_id>**
+
+Delete a user with the specified ID.
+
+```
+curl -X DELETE http://localhost:5000/users/<user_id>
+```
+
+## License
+
+This project is released under the [MIT License](https://choosealicense.com/licenses/mit/).
---
+++ main.py
@@ -0,0 +1,122 @@
+# Import Libraries
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
+# Create Flask application
+app = Flask(__name__)
+
+# Configure database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the SQLAlchemy object
+db = SQLAlchemy(app)
+
+
+# Define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+
+# Create the tables of the database
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+# Route to get all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    # Query the database to get all users
+    users = User.query.all()
+
+    # Return a JSON response with the data of all users
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
+
+
+# Route to get user by ID
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    # Retrieve user object with the given id
+    user = User.query.get(user_id)
+    # Check if user exists, return 404 error if not
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    # Return user information in JSON format
+    return jsonify({'id': user.id, 'name': user.name, 'email': user.email})
+
+
+# Route to handle POST requests for creating users
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Get JSON data from request
+    data = request.get_json()
+    # Check if the user already exists
+    if User.query.filter_by(email=data['email']).first():
+        return jsonify({'message': 'User already exists!'}), 400
+    # Create new User object with data
+    new_user = User(name=data['name'], email=data['email'],
+                    password=generate_password_hash(data['password'], method='sha256'))
+    # Add new user to database session
+    db.session.add(new_user)
+    # Commit changes to database
+    db.session.commit()
+    # Return success message
+    return jsonify({'message': 'User created successfully!'})
+
+
+# Route to handle PUT requests for updating user info
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    # Get the user with the specified ID from the database
+    user = User.query.get(user_id)
+    # If the user is not found, return a 404 error response
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    # Parse request data as JSON
+    data = request.get_json()
+    # Update user's name, email, and password with new values
+    user.name = data['name']
+    user.email = data['email']
+    user.password = data['password']
+    # Commit changes to database
+    db.session.commit()
+    # Return success message
+    return jsonify({'message': 'User updated successfully!'})
+
+
+# Route to handle DELETE requests for deleting user
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    # Get the user with the specified ID from the database
+    user = User.query.get(user_id)
+    # If the user is not found, return a 404 error response
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    # Detach user from the current session
+    db.session.expunge(user)
+    # Delete user from database
+    try:
+        db.session.delete(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'message': 'Failed to delete user: {}'.format(str(e))}), 500
+    # Return success message
+    return jsonify({'message': 'User deleted successfully!'})
+
+
+# Run application if file is run as main
+if __name__ == '__main__':
+    app.run(debug=True)
